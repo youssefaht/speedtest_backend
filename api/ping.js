9452aa3217150415a3d4a855ee950dc12f8f6a81
youssefaht <youssefaht25@gmail.com>
2026-06-08 06:04:00 +0100

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,14 @@
+// api/ping.js
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
+  // 204 = no body = fastest possible response = أدق ping
   res.status(204).end();
 }
